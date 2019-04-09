2740fc8d0e1612885784cbd9a9317582937e9e6f
linmaowei <linmaowei@cvte.com>
2019-04-09 07:42:13 +0100

    fix use-emacs.sh

--- use-emacs.sh
+++ use-emacs.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-TAR_EMACS_D="emacs.d.tar.gz"
-TAR_MYELPA="myelpa.tar.gz"
-POS_EMACS_D="~/.emacs.d"
-POS_MYELPA="~/myelpa"
+yum install -y emacs
+
+TAR_EMACS_D=emacs.d.tar.gz
+TAR_MYELPA=myelpa.tar.gz
+POS_EMACS_D=~/.emacs.d
+POS_MYELPA=~/myelpa
 
 wget https://github.com/godmao/emacs.d/archive/emacs24.3.tar.gz -O $TAR_EMACS_D
 wget https://github.com/godmao/myelpa/archive/emacs24.3.tar.gz -O $TAR_MYELPA
 
-mv ~/.emacs.d ~/.emacs.d_bak
+if [ -d $POS_EMACS_D ]; then
+    mv $POS_EMACS_D ${POS_EMACS_D}_bak
+fi
+
 mkdir $POS_EMACS_D
 mkdir $POS_MYELPA
 
-tar zxvf $TAR_EMACS_D -C $POS_EMACS_D --strip-components=1
-tar zxvf $TAR_MYELPA -C $POS_MYELPA --strip-components=1
+tar zxf $TAR_EMACS_D -C $POS_EMACS_D --strip-components=1
+tar zxf $TAR_MYELPA -C $POS_MYELPA --strip-components=1
 
+rm -f $TAR_EMACS_D
+rm -f $TAR_MYELPA
